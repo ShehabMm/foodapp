19ef82d4fada48be1de50a7afcd703619f6359a3
shehab <shehab.mm@gmail.com>
2024-11-02 23:39:29 +0200

    swap

--- foodapp/src/Pages/New.jsx
+++ foodapp/src/Pages/New.jsx
@@ -20,26 +20,69 @@ function New() {
     { title1: "C" },
   ]);
 
-const handleChecked=(e)=>{
+const handleChecked=(index)=>{
 
-const updated = array1.map((item,index)=>index==e?[...array1,item.checked==true  ]:array1  )
-setarray1(updated)
+const updated = [...array1];
+ updated[index].checked=!updated[index].checked;
+ setarray1(updated)
 
 }
 
+const handleC =()=>{
+  const updated = [...array1];
+const updated2=[...array2]
+
+updated.forEach((item, index)=>{
+
+if (item.checked){
+const tempo = updated[index].title1
+updated[index].title1=updated2[index].title1
+updated2[index].title1=tempo
+}
+
+})
+setarray1(updated)
+setarray2(updated2)
+
+
+}
+
+
+
+   
+  
+
+
+
+
+
   return (
-    <div  >
+    <div >
       {array1.map((item, index) => {
         return (
           <ul key={index}>
            <li>
-                <input type="checkbox" checked={item.checked}  onChange={()=>{handleChecked({index})}} value={name}/>
+                <input type="checkbox" checked={item.checked}  onChange={()=>{handleChecked(index)}} value={name}/>
                 {item.title1}
                 </li>
           </ul>
+
         );
       })}
+
+{array2.map((item)=>{
+
+return (
     
+    
+   <h3>    {item.title1}     </h3>
+   
+)
+
+})}
+
+                              <button onClick={()=>{handleC()}}>Swap wow </button>
+
       <select>
 
         <option value="">tezo</option>
